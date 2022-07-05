5d839e55d7428685127a23f30fbd33f054ec0407
jbep <joshuabemartin@gmail.com>
2022-07-05 19:46:25 -0400

    created firegotchi class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('hello')
+class Firegotchi {
+  constructor(name) {
+    this.name = name;
+    this.hunger = 1;
+    this.sleepiness = 1;
+    this.boredom = 1;
+    this.age = 0;
+  }
+}
+const pet = new Firegotchi('your pet')
+console.log(pet)
