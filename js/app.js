524db0d179fcdecb09cf8a4dd34f6fac36a87fb3
jbep <joshuabemartin@gmail.com>
2022-07-05 23:13:23 -0400

    fonished functions within class

--- js/app.js
+++ js/app.js
@@ -1,41 +1,44 @@
 //class for tamagotchi
-const totalCount = document.querySelector('#total-count');
 class Firegotchi {
   constructor(name) {
     this.name = name;
     this.hunger = 1;
-    this.sleepiness = 1;
-    this.boredom = 1;
+    this.sleep = 1;
+    this.bored = 1;
     this.age = 0;
   }
   ageUp () {
-    let age = this.age
-    if (age < 30) {
-      console.log('ageup')
-      age++
-      return age
-    }
-    setTimeout(ageUp, 2000);
+  this.age++
+  console.log(this.age);
   }
   feedPet () {
-    this.hunger--;
+    this.hunger --;
+    if (this.hunger < 0) {
+      this.hunger = 0;
+    }
     console.log(this.hunger);
     }
-  sleep() {
-    this.sleepiness --
+  sleepy () {
+    this.sleep --;
+    console.log(this.sleep);
   }
-  bored1() {
-    this.bored --
+  bored1 () {
+    this.bored --;
+    console.log(this.bored);
   }
 }
 const pet = new Firegotchi('your pet')
 // console.log(pet.ageUp());
+pet.ageUp();
+pet.feedPet();
+pet.sleepy();
+pet.bored1();
+console.log(pet);
 
 // const displayAge = () => {
 //
 // }
-  const count = pet.hunger;
-  totalCount.innerText = count;
-  document.getElementById('feed').addEventListener("click", pet.feedPet())
+
+document.getElementById('feed').addEventListener('click', e => pet.feedPet)
 // document.getElementById('lights').addEventListener('click', )
 // document.getElementById('bored').addEventListener('click', )
